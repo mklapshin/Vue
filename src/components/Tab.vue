dcca1f8181fd6e394003d3ab6d0dc86b875a25c2
mklapshin <golub.97@mail.ru>
2021-12-21 17:53:47 +0300

    working in module styles

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -19,78 +19,72 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" module>
 @import "../assets/scss/main.scss";
-.todo-app__footer {
-  .app-footer {
-    &__tabs {
-      &__label {
-        cursor: pointer;
-        border: 0.125rem solid $peachOrange;
-        border-radius: 0.25rem;
-        &:hover {
-          border-color: $carrotOrange;
-          &::after {
-            background-color: $carrotOrange;
-          }
-          &::before {
-            background-color: $carrotOrange;
-          }
-        }
-        padding: 0.25rem 0.625rem;
 
-        font-size: 1.25rem;
-        line-height: 1.75rem;
-        color: $twineOpacity;
-        background-color: transparent;
-        border: 0.125rem solid transparent;
-        border-radius: 0.625rem;
-        cursor: pointer;
-        transition: border-color 0.5s, color 0.5s;
-        &:not(:last-child) {
-          margin-right: 1.5rem;
-        }
-      }
+.appTabsLabel {
+  cursor: pointer;
+  border: 0.125rem solid $peachOrange;
+  border-radius: 0.25rem;
+  &:hover {
+    border-color: $carrotOrange;
+    &:after {
+      background-color: $carrotOrange;
     }
+    &:before {
+      background-color: $carrotOrange;
+    }
+  }
+  padding: 0.25rem 0.625rem;
+
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  color: $twineOpacity;
+  background-color: transparent;
+  border: 0.125rem solid transparent;
+  border-radius: 0.625rem;
+  cursor: pointer;
+  transition: border-color 0.5s, color 0.5s;
+  &:not(:last-child) {
+    margin-right: 1.5rem;
   }
 }
-.app-tabs {
-  &__input {
-    display: none;
-  }
 
-  &__fake {
+.appTabsInput {
+  display: none;
+}
+
+.appTabsFake {
+  cursor: pointer;
+  display: inline-block;
+  width: 5.3rem;
+  height: 0.875rem;
+  margin: 0 0.2rem 0 0;
+  text-align: center;
+  border-radius: 0.25rem;
+  position: relative;
+
+  &:before {
     cursor: pointer;
-    display: inline-block;
-    width: 5.3rem;
-    height: 0.875rem;
-    margin: 0 0.2rem 0 0;
-    text-align: center;
-    border-radius: 0.25rem;
-    position: relative;
-
-    &::before {
-      cursor: pointer;
-      content: "";
-      position: absolute;
-      display: block;
-      width: 5.7rem;
-      margin: 0 0 0 0.3rem;
-      height: 2rem;
-      border-radius: 0.7rem;
-      border: 0.2rem solid $rajah;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      opacity: 0;
-      transition: 0.2s;
-      z-index: 3;
-    }
+    content: "";
+    position: absolute;
+    display: block;
+    width: 5.7rem;
+    margin: 0 0 0 0.3rem;
+    height: 2rem;
+    border-radius: 0.7rem;
+    border: 0.2rem solid $rajah;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    transition: 0.2s;
+    z-index: 3;
   }
+}
 
-  .app-tabs__input:checked + .app-tabs__fake::before {
-    opacity: 1;
-    cursor: pointer;
-  }
+.appTabsInput:checked + .appTabsFake:before {
+  opacity: 1;
+  cursor: pointer;
 }
 </style>
